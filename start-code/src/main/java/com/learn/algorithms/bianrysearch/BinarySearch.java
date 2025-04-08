bf1fb0ed757e401825c8f8bd8c757ef4248c5126
swwithzhao <1293342954@qq.com>
2025-04-08 21:40:35 +0800

    基础版二分查找

--- start-code/src/main/java/com/learn/algorithms/bianrysearch/BinarySearch.java
+++ start-code/src/main/java/com/learn/algorithms/bianrysearch/BinarySearch.java
@@ -1,4 +1,19 @@
 package com.learn.algorithms.bianrysearch;
 
 public class BinarySearch {
+
+    public static int binarySearchBasic(int[] a, int target) {
+        int left = 0, right = a.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target>a[mid]){
+                left=mid+1;
+            }else if (target<a[mid]){
+                right=mid-1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
---
+++ start-code/src/test/java/com/learn/algorithms/bianrysearch/TestBinarySearch.java
@@ -0,0 +1,28 @@
+package com.learn.algorithms.bianrysearch;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.learn.algorithms.bianrysearch.BinarySearch.binarySearchBasic;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBinarySearch {
+
+    @Test
+    @DisplayName("二分查找基础版")
+    public void testBinarySearchBasic(){
+        int[] a = {7, 13, 21, 30, 38, 44, 52, 53};
+        assertEquals(0, binarySearchBasic(a, 7));
+        assertEquals(1, binarySearchBasic(a, 13));
+        assertEquals(2, binarySearchBasic(a, 21));
+        assertEquals(3, binarySearchBasic(a, 30));
+        assertEquals(4, binarySearchBasic(a, 38));
+        assertEquals(5, binarySearchBasic(a, 44));
+        assertEquals(6, binarySearchBasic(a, 52));
+        assertEquals(7, binarySearchBasic(a, 53));
+
+        assertEquals(-1, binarySearchBasic(a, 0));
+        assertEquals(-1, binarySearchBasic(a, 15));
+        assertEquals(-1, binarySearchBasic(a, 60));
+    }
+}
